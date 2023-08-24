0eedfb488b76279b061d4a6a4b40b6ee5a4c4831
Peter <>
2023-08-25 00:05:25 +0200

    app_giphy-world-vers_1

--- lib/giphy_demo.dart
+++ lib/giphy_demo.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-
-import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter/material.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:velocity_x/velocity_x.dart';
 import 'giphy_demo.dart';
 
 void main() => runApp(const MyApp());
