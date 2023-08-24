9249387ee647b83dd1dfc2a11fc83c9e28146dcd
Peter <>
2023-08-24 22:11:36 +0200

    upd_api-implemented

--- lib/giphy_demo.dart
+++ lib/giphy_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:velocity_x/velocity_x.dart';
@@ -10,6 +12,21 @@ class GiphyPage extends StatefulWidget {
 
 class _GiphyPageState extends State<GiphyPage> {
   final TextEditingController controller = TextEditingController();
+
+  var data;
+
+  final String url =
+      "https://api.giphy.com/v1/gifs/search?api_key=imv0MqAoW8tJ2RijqNF94vK5D4xk8mz2&limit=25&offset=0&rating=G&lang=en&q=";
+  void fetchData(String searchInput) async {
+    Uri uri = Uri.parse(url + searchInput);
+
+    http.Response res = await http.get(uri);
+    data = jsonDecode(res.body)["data"];
+    setState(() {});
+
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,12 +49,15 @@ class _GiphyPageState extends State<GiphyPage> {
             child: Center(
               child: Column(
                 children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
                   Row(
                     children: [
                       Expanded(
                         child: TextField(
                           controller: controller,
-                          decoration: InputDecoration(
+                          decoration: const InputDecoration(
                             focusedBorder: OutlineInputBorder(
                               borderSide: BorderSide(color: Color(0xFFFF3A46)),
                             ),
@@ -56,12 +76,37 @@ class _GiphyPageState extends State<GiphyPage> {
                                   Colors.black87),
                               fixedSize: MaterialStateProperty.all<Size>(
                                   Size(50, 50))),
-                          onPressed: () {},
+                          onPressed: () {
+                            fetchData(controller.text);
+                          },
                           child: Text('GO!'),
                         ),
-                      )
+                      ),
                     ],
                   ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  VxConditional(
+                    condition: data != null,
+                    builder: (context) => GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: context.isMobile ? 2 : 3),
+                      itemBuilder: (context, index) {
+                        final url =
+                            data[index]["images"]["fixed_height"]["url"];
+                        return Text(url);
+                      },
+                      itemCount: data.length,
+                    ),
+                    fallback: (context) => const Text(
+                      'Nothing Found',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ).h(context.percentHeight * 60),
                 ],
               ),
             ),
