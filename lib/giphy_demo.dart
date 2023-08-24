489cd5afb83e64fcaa3f3c50b09be1757899834d
Peter <>
2023-08-24 20:26:37 +0200

    feat_search-button

--- lib/giphy_demo.dart
+++ lib/giphy_demo.dart
@@ -1,15 +1,15 @@
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter/material.dart';
 
 class GiphyPage extends StatefulWidget {
-  const GiphyPage({super.key});
-
   @override
   State<GiphyPage> createState() => _GiphyPageState();
 }
 
 class _GiphyPageState extends State<GiphyPage> {
+  final TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +26,41 @@ class _GiphyPageState extends State<GiphyPage> {
       ),
       body: Theme(
         data: ThemeData.dark(),
-        child: const SafeArea(
+        child: SafeArea(
           child: Padding(
-            padding: EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(20.0),
             child: Center(
               child: Column(
                 children: [
-                  TextField(
-                    decoration: InputDecoration(),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: controller,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFFF3A46)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.cyan),
+                            ),
+                            label: Text('Search Giphy'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.black87),
+                              fixedSize: MaterialStateProperty.all<Size>(
+                                  Size(50, 50))),
+                          onPressed: () {},
+                          child: Text('GO!'),
+                        ),
+                      )
+                    ],
                   ),
                 ],
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const GiphyPage(),
+      home: GiphyPage(),
     );
   }
 }
